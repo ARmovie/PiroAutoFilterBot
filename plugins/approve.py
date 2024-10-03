59236343f64a2edf2a60c42bb003b5de47c0c2f8
Dora public bots <106464152+ARmovie@users.noreply.github.com>
2024-10-03 22:13:21 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -14,7 +14,7 @@ async def accept_request(client, r):
     try:
         await client.send_photo(
             r.from_user.id,
-            'https://envs.sh/WkH.jpg',
+            'https://telegra.ph/file/b5c44b091d4c2845abb40.jpg',
             f"**𝖧𝖾𝗅𝗅𝗈 {r.from_user.mention} 👻, 𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝖳𝗈 {r.chat.title}\n𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖠𝗉𝗉𝗋𝗈𝗏𝖾𝖽...!!!**",
             reply_markup=rm)
 
